48e3e002c0a088bc591d6fea36cc88bb0b0c34d1
Abdulla554 <163930376+Abdulla554@users.noreply.github.com>
2024-04-15 05:22:32 +0300

    first commit

---
+++ git1/src/components/2-hero/Hero.jsx
@@ -0,0 +1,69 @@
+import Lottie from "lottie-react";
+import "./hero.css";
+import devAnimation from "../../animation/dev.json";
+import head from "../../animation/head.json";
+import { useRef } from "react";
+import { motion } from "framer-motion";
+
+const Hero = () => {
+  const lottieRef = useRef();
+
+  return (
+    <section className="hero flex">
+      <div className="left-section  ">
+        <div className="parent-avatar flex">
+          <motion.img
+            initial={{ transform: "scale(0)" }}
+            animate={{ transform: "scale(1.1)" }}
+            transition={{ damping: 6, type: "spring", stiffness: 100 }}
+            src="./11.jpg"
+            className="avatar"
+            alt=""
+          />
+          <div className="icon-verified"></div>
+        </div>
+
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+          className="title"
+        >
+          Software designer, founder, and amateur astronaut.
+        </motion.h1>
+
+        <p className="sub-title">
+          I’m ABDULLA SAMER, a software designer and entrepreneur based in New York
+          City. I’m the founder and CEO of Planetaria, where we develop
+          technologies that empower regular people to explore space on their own
+          terms.
+        </p>
+
+        <div className="all-icons flex">
+          <div className="icon icon-twitter"></div>
+          <div className="icon icon-instagram"></div>
+          <div className="icon icon-github"></div>
+          <div className="icon icon-linkedin"></div>
+        </div>
+      </div>
+
+      <div className="right-section animation ">
+        {/* <Lottie
+          lottieRef={lottieRef}
+          className=""
+          onLoadedImages={() => {
+            // @ts-ignore
+            // https://lottiereact.com/
+            lottieRef.current.setSpeed(0.5);
+          }}
+          animationData={devAnimation}
+        /> */}
+         <Lottie style={{ height:300,marginLeft:-70,marginTop:-170 }}
+          animationData={ head}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ git1/src/components/3-main/Main.jsx
@@ -0,0 +1,128 @@
+import { useState } from "react";
+import "./main.css";
+import { myProjects } from "./myProjects";
+import { AnimatePresence, motion } from "framer-motion";
+
+const Main = () => {
+  const [currentActive, setcurrentActive] = useState("all");
+  
+  const [arr, setArr] = useState(myProjects);
+
+  const handleClick = (buttonCategory) => {
+    setcurrentActive(buttonCategory);
+
+    const newArr = myProjects.filter((item) => {
+      const ZZZ = item.category.find((myItem) => {
+        return myItem === buttonCategory;
+      });
+
+      return ZZZ === buttonCategory;
+    });
+
+    setArr(newArr);
+  };
+
+  return (
+    <main className="flex">
+      <section className="flex  left-section">
+        <button
+        //  const [currentActive, setcurrentActive] = useState("all");
+//  stateعندي قيمتها الابتدائيه  allزين 
+// 
+// 
+//         
+          onClick={() => {
+            // من اضغط غيرلي قيمت الاستيت شنو هيه بيها الى الل ف مثلا جانت قيمت الاستيت جافا ف من راح ادوس عليها راح تصير الل ف يشتغل شرط الاكتف
+            setcurrentActive("all");
+            setArr(myProjects);
+          }}
+          // اذا الاستيت تساوي الكل خليها اكتف واذا لا لاتخليها
+          className={currentActive === "all" ? "active" : null}
+        >
+          all projects
+        </button>
+
+        <button
+        //       من اضغط غيريلي قيمت الاستيت الى سي س س فراح تصير قيمت الاستيت سي ااس ف راح تنزل للشرط تشوفه متوافق فيتحققق
+          onClick={() => {
+            handleClick("css");
+          }}
+          className={currentActive === "css" ? "active" : null}
+        >
+          HTML & CSS
+        </button>
+
+        <button
+          onClick={() => {
+            handleClick("js");
+          }}
+          className={currentActive === "js" ? "active" : null}
+        >
+          JavaScript
+        </button>
+        <button
+          onClick={() => {
+            handleClick("react");
+          }}
+          className={currentActive === "react" ? "active" : null}
+        >
+          React & MUI
+        </button>
+        <button
+          onClick={() => {
+            handleClick("node");
+          }}
+          className={currentActive === "node" ? "active" : null}
+        >
+          Node & Express
+        </button>
+      </section>
+
+
+
+      <section className=" flex right-section">
+        <AnimatePresence>
+          {arr.map((item) => {
+            return (
+              <motion.article
+                layout
+                initial={{ transform: "scale(0.4)" }}
+                animate={{ transform: "scale(1)" }}
+                transition={{ type: "spring", damping: 8, stiffness: 50 }}
+                key={item.imgPath}
+                className="  card"
+              >
+                <img width={266} src={item.imgPath} alt="" />
+
+                <div style={{ width: "266px" }} className="box">
+                  <h1 className="title">{item.projectTitle}</h1>
+                  <p className="sub-title">
+                    Lorem ipsum dolor sit amet consectetur elit adipisicing . Ex
+                    tempore dolor in, accusantium laudantium accusamus.
+                  </p>
+
+                  <div className="flex icons">
+                    <div style={{ gap: "11px" }} className="flex">
+                      <div className="icon-link"></div>
+                      <div className="icon-github"></div>
+                    </div>
+
+                    <a className="link flex" href="">
+                      more
+                      <span
+                        style={{ alignSelf: "end" }}
+                        className="icon-arrow-right"
+                      ></span>
+                    </a>
+                  </div>
+                </div>
+              </motion.article>
+            );
+          })}
+        </AnimatePresence>
+      </section>
+    </main>
+  );
+};
+
+export default Main;
